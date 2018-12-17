9d42269316e979eff3cd3bf224e4c8e9cc15b975
alexandr <A.A.Plekhanov>
2018-12-17 18:35:26 +0300

    added some request

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -3,18 +3,31 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
 
-    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+
+    // если метод возвращает не String , то имя для view будет равно маппингу
+    @RequestMapping(path = {"/hello"}, method = RequestMethod.GET)
     public String hello(Model model){
 
         model.addAttribute("message", "Hello world from Controller");
+        model.addAttribute("Max");
+        model.addAttribute("Mike");
+
         return "hello";
     }
+
+    @RequestMapping("/hello2")
+    @ResponseBody
+    // @RequestParam int integer - имя параметра integer
+    public String hello2(@RequestParam("first") String first , @RequestParam(required = false, defaultValue = "default") String second, @RequestParam int integer) {
+        return first + second + integer;
+    }
+
+
 
 
 }
