05b1f6d46107f7987e7ee496eeddd815661d6655
alexandr <A.A.Plekhanov>
2018-12-18 19:51:05 +0300

    added some servlets

--- src/main/java/config/AppInit.java
+++ src/main/java/config/AppInit.java
@@ -1,43 +1,47 @@
-//package config;
-//
-//
-//import org.springframework.web.WebApplicationInitializer;
-//import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-//import org.springframework.web.servlet.DispatcherServlet;
-//import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-//
-//import javax.servlet.ServletContext;
-//import javax.servlet.ServletException;
-//import javax.servlet.ServletRegistration;
-//
-///**
-// * инициализация Spring MVC через аннотации ( Файлов dispatcher-servlet.xml и web.xml не должно быть чтобы не было конфликтов)
-// */
-//
-//
-//
-//public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
-//    // Этот метод должен содержать конфигурации которые инициализируют Beans
-//    // для инициализации бинов у нас использовалась аннотация @Bean
-//    @Override
-//    protected Class<?>[] getRootConfigClasses() {
-//        return new Class<?>[]{
-//                MyWebConfig.class
-//        };
-//    }
-//
-//    // Тут добавляем конфигурацию, в которой инициализируем ViewResolver
-//    @Override
-//    protected Class<?>[] getServletConfigClasses() {
-//
-//        return new Class<?>[]{
-//                MyWebConfig.class
-//        };
-//    }
-//
-//    @Override
-//    protected String[] getServletMappings() {
-//        return new String[]{"/"};
-//    }
-//
-//}
+package config;
+
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+/**
+ * инициализация Spring MVC через аннотации ( Файлов dispatcher-servlet.xml и web.xml не должно быть чтобы не было конфликтов)
+ */
+
+
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    // Этот метод должен содержать конфигурации которые инициализируют Beans
+    // для инициализации бинов у нас использовалась аннотация @Bean
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    // Тут добавляем конфигурацию, в которой инициализируем ViewResolver
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+
+        return new Class<?>[]{
+                MyWebConfig.class, MyWebConfig.class
+        };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        // путь сохранения файлов
+      registration.setMultipartConfig(new MultipartConfigElement("/tmp/app/uploads"));
+    }
+}
