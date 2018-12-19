1ee0dae7c07e584fa4bcc7472ab8b298f5233806
A.A.Plekhanov <A.A.Plekhanov@rambler.ru>
2018-12-19 23:47:17 +0300

    added Spring data

---
+++ src/main/java/config/DbConfigMY.java
@@ -0,0 +1,32 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DbConfigMY {
+
+    @Bean
+    public DataSource getDataSource () {
+
+
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+
+        driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/Lessons?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
+        driverManagerDataSource.setUsername("root");
+        driverManagerDataSource.setPassword("root");
+
+        return driverManagerDataSource;
+    }
+
+
+    @Bean
+    public JdbcTemplate getJdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
---
+++ src/main/java/springData/MySpringDataConfig.java
@@ -0,0 +1,34 @@
+package springData;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("springData")
+public class MySpringDataConfig {
+
+     @Bean
+    public DataSource getDataSource () {
+
+
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+
+        driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/Lessons?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
+        driverManagerDataSource.setUsername("root");
+        driverManagerDataSource.setPassword("root");
+
+        return driverManagerDataSource;
+    }
+    @Bean
+    public JdbcTemplate getJdbcTemplate(DataSource dataSource) {
+         return new JdbcTemplate(dataSource);
+    }
+
+}
