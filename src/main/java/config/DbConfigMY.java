c1bda4805fc8abd058c705b9a19f64ab5608ced2
A.A.Plekhanov <A.A.Plekhanov@rambler.ru>
2019-01-02 21:34:16 +0300

    added namedGetBook servlet , set 8 target and source compiler for Jetty plugin and project

--- src/main/java/config/DbConfigMY.java
+++ src/main/java/config/DbConfigMY.java
@@ -3,6 +3,8 @@ package config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jndi.JndiObjectFactoryBean;
 
@@ -38,5 +40,11 @@ public class DbConfigMY {
     @Bean
     public JdbcTemplate getJdbcTemplate(DataSource dataSource) {
         return new JdbcTemplate(dataSource);
+    }
+
+
+    @Bean
+    public NamedParameterJdbcOperations getNamedParameterJdbcOperations(DataSource dataSource) {
+        return new NamedParameterJdbcTemplate(dataSource);
     }
 }
--- src/main/java/controllers/DbController.java
+++ src/main/java/controllers/DbController.java
@@ -3,20 +3,28 @@ package controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import other.Book;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class DbController {
 
     @Autowired
      JdbcOperations jdbcOperations;
+
+
+    @Autowired
+    NamedParameterJdbcOperations namedParameterJdbcOperations;
 
     @RequestMapping("/createTable")
     @ResponseBody
@@ -32,6 +40,18 @@ public class DbController {
     public String getBook(@PathVariable ("id") String id) {
        Book book =  jdbcOperations.queryForObject("select * from books where id =" +id, new BookMapper());
        return book.toString();
+    }
+
+    // возвращает книгу из БД по id с помощью именовоного параметра
+    @RequestMapping("/namedGetBook/{id}")
+    @ResponseBody
+    public String namedGetBook(@PathVariable("id") String id) {
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("idBookFromMap", id);
+        Book book = namedParameterJdbcOperations.queryForObject("select * from books where id = :idBookFromMap",map , new BookMapper());
+
+        return book.toString();
     }
 
 
