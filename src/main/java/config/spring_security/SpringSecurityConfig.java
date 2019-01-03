04ed15aa37ef647bd3f6af9aef401be8ad4f1934
A.A.Plekhanov <A.A.Plekhanov@rambler.ru>
2019-01-04 02:00:09 +0300

    added Spring security config

--- src/main/java/config/spring_security/SpringSecurityConfig.java
+++ src/main/java/config/spring_security/SpringSecurityConfig.java
@@ -1,10 +1,19 @@
 package config.spring_security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 
 /**
@@ -16,11 +25,31 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("user").password("{noop}user").roles("user").and()
                 .withUser("admin").password("{noop}admin").roles("admin");
+
+/*
+        если хотим хранить логины и пароли в БД
+
+   ->    auth.jdbcAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance()).dataSource(dataSource);
+
+        логины и пароли хранятся в следующих таблицах
+
+        create table users(
+        username  varchar(50) not null primary key,
+        password varchar(500) not null,
+        enabled boolean not null);
+
+        create table authorities (
+        username varchar(50) not null primary key,
+        authority varchar(50) not null);
+*/
     }
 
     @Override
